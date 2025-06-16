ab17a05c7419da5bd2bf042df9b14c5dbd2932ba
Priscilla Erickson <priscilla.erickson@gmail.com>
2025-06-16 09:46:54 -0400

    Delete not used directory

--- not used/baypass_window_plotting.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=20
-#SBATCH --mem=100G
-#SBATCH --time=10:00:00
-#SBATCH --partition=basic
-
-Rscript /scratch/perickso/private/ind_seq/popgen/scripts/baypass_window_plotting.R
